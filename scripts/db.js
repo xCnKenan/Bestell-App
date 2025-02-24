7cea0d0b062625b4e26ac381dbc6828679398c16
xCnKenan <cn.kenan@icloud.com>
2025-02-24 12:27:56 +0100

    first commit

---
+++ scripts/db.js
@@ -0,0 +1,152 @@
+let mainDishes = [
+    {
+        "name":"Veggie Buddha Bowl",
+        "price": 14.99,
+        "description":"mit gegrilltem Gemüse, Süßkartoffel Chunks, Roter Bete, Edamame, Walnüssen, warmen Quinoa & Vollkornreis und frischem Salat.",
+    },
+    {
+        "name":"Japanese Beef Bowl",
+        "price": 12.99,
+        "description":"mit frisch gegrilltem Weiderind, Rotkohl, Gurken, fruchtiger Mango, Miso Mayo, gerösteten Erdnüssen, frischem Koriander, Jasmin-Duftreis und frischem Salat.",
+    },
+    {
+        "name":"Vegan Falafel Bowl",
+        "price": 17.99,
+        "description":"mit Green Falafel, cremigem Hummus, saftigen Granatapfelkernen, Roter Bete, Gurken, knackigen Karotten, frischer Minze, warmen Quinoa & Vollkornreis und frischem Salat.",
+    },
+    {
+        "name":"Crunchy Chicken Bowl",
+        "price": 14.99,
+        "description":"mit gegrillten Hähnchen-Filetstreifen, Edamame, sonnengereiften Kirschtomaten, fruchtiger Mango, saftigen Granatapfelkernen, gerösteten Crunchy Onions, warmen Quinoa & Vollkornreis und frischem Salat.",
+    },
+    {
+        "name":"Black Rice Avocado Chicken Bowl",
+        "price": 13.99,
+        "description":"mit gegrillten Hähnchen-Filetstreifen, Avocado,  Babyspinat, sonnengereiften Kirschtomaten, Spitzpaprika, schwarzem Reis, frischem Koriander, Lemon Herbs Sauce.",
+    },
+    {
+        "name":"Chicken Teriyaki Bowl",
+        "price": 12.99,
+        "description":"mit gegrillten Hähnchen-Filetstreifen, Rotkohl, sonnengereiften Kirschtomaten, Gurken, Frühlingszwiebeln, Jasmin-Duftreis, Sesam und Limette.",
+    },
+    {
+        "name":"Avocado Chicken Bowl",
+        "price": 16.99,
+        "description":"mit frisch gegrilltem Hähnchen-Filet, Avocado, knackiger Paprika, sonnengereiften Kirschtomaten, frischem Koriander, Frühlingszwiebeln, Limette, warmen Quinoa & Vollkornreis und frischem Salat.",
+    }
+];
+
+let basket = [
+    {
+        "price": 14.99,
+        "amount": 0,
+        "newPrice": 0  
+    },
+    {
+        "price": 12.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 17.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 14.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 13.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        
+        "price": 12.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 16.99,
+        "amount": 0,
+        "newPrice": 0
+    }
+];
+
+let sweetDishes = [
+    {
+        "name":"Himbeer Mandel Brownie",
+        "price": 6.99,
+        "description":"mit Schokolade und Haselnüssen"
+    },
+    {
+        "name":"Bircher Müsli",
+        "price": 5.99,
+        "description":"mit Granatapfelkernen"
+    },
+    {
+        "name":"Chocolate Banana Bowl",
+        "price": 7.99,
+        "description":"mit Mango, Rote Bete, Banane, Haferdrink und Kakao"
+    },
+    {
+        "name":"Tropical Mango Bowl",
+        "price": 9.99,
+        "description":"mit Mango, Ananas, Süßkartoffel, Banane, Kurkuma, Haferdrink und Zitronensaft"
+    },
+    {
+        "name":"Açaí Bowl",
+        "price": 7.99,
+        "description":"mit Erdbeeren, roten Beeren, Açaí, Banane, Haferflocken und Mandeln"
+    },
+    {
+        "name":"Bananenbrot",
+        "price": 8.99,
+        "description":"mit Johannisbeeren, Walnüssen, Mandeln"
+    },
+    {
+        "name":"Carrot Cake",
+        "price": 6.99,
+        "description":"mit Frischkäse-Topping und Pistazien"
+    },
+];
+
+let basketSweet = [
+    {
+        "price": 6.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 5.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 7.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 9.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 7.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 8.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+    {
+        "price": 6.99,
+        "amount": 0,
+        "newPrice": 0
+    },
+];
